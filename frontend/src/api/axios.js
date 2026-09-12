41a5dc014244cb089c1a9a3063c07fa1a029eb5c
bobyrpl <bobyrpl348@gmail.com>
2026-09-12 16:43:35 +0700

    5

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://app-parkir-production.up.railway.app//api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://app-parkir-production.up.railway.app/api',
     headers: {
         Accept: 'application/json',
     },
